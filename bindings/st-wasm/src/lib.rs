e271e333e25cd50aaa1fce2f76c7dc133e996ba4
Ryo ∴ SpiralArchitect <kishkavsesvit@icloud.com>
2025-10-22 02:42:06 -0700

    Allow COBOL planners to import saved envelopes

--- bindings/st-wasm/src/lib.rs
+++ bindings/st-wasm/src/lib.rs
@@ -1,7 +1,11 @@
 #![cfg_attr(not(target_arch = "wasm32"), allow(dead_code))]
 
+mod cobol;
+
 #[cfg(target_arch = "wasm32")]
 mod canvas;
+#[cfg(target_arch = "wasm32")]
+mod cobol_bridge;
 #[cfg(target_arch = "wasm32")]
 mod fft;
 #[cfg(target_arch = "wasm32")]
@@ -11,6 +15,9 @@ mod utils;
 
 #[cfg(target_arch = "wasm32")]
 pub use canvas::*;
+pub use cobol::*;
+#[cfg(target_arch = "wasm32")]
+pub use cobol_bridge::*;
 #[cfg(target_arch = "wasm32")]
 pub use fft::*;
 #[cfg(target_arch = "wasm32")]
