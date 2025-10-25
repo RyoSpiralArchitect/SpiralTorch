5db267cc64f88ff3e8c576953b25bc67fa84fad5
Ryo ∴ SpiralArchitect <kishkavsesvit@icloud.com>
2025-10-26 06:39:32 +0900

    Add hypergrad session helpers for Python wheel

--- bindings/python/spiral/__init__.py
+++ bindings/python/spiral/__init__.py
@@ -1,24 +1,49 @@
-"""Lightweight Python utilities that complement the compiled bindings."""
-
-from .data import augment
-
-__all__ = ["augment"]
 """High level SpiralTorch Python interfaces."""
 
+from .data import augment
+from .data.augment import (
+    gaussian_noise,
+    normalize_batch,
+    random_crop,
+    random_mask,
+    solarize,
+)
+from .hypergrad import (
+    hypergrad_session,
+    hypergrad_summary_dict,
+    suggest_hypergrad_operator,
+)
 from .inference import (
     AuditEvent,
     AuditLog,
+    ChatMessage,
+    ChatPrompt,
     InferenceClient,
     InferenceResult,
+    SafetyEvent,
     SafetyVerdict,
     SafetyViolation,
+    format_chat_prompt,
 )
 
 __all__ = [
+    "augment",
+    "gaussian_noise",
+    "normalize_batch",
+    "random_crop",
+    "random_mask",
+    "solarize",
     "AuditEvent",
     "AuditLog",
+    "ChatMessage",
+    "ChatPrompt",
     "InferenceClient",
     "InferenceResult",
+    "SafetyEvent",
     "SafetyVerdict",
     "SafetyViolation",
+    "format_chat_prompt",
+    "hypergrad_session",
+    "hypergrad_summary_dict",
+    "suggest_hypergrad_operator",
 ]
